365c66ea6bb2cdd085b3b2dd32cc2e4130ffc45f
GriseldaZBalmaceda <gzbalmaceda@my365.bellevue.edu>
2019-05-12 22:18:17 -0500

    adding more code attribution

--- api-gateway/models/user.js
+++ api-gateway/models/user.js
@@ -2,13 +2,14 @@
 //creating userSchema
 var mongoose = require('mongoose');
 
-
+//userSchema incudes  username,password and email
 var userSchema = new mongoose.Schema({
     username: String,
     password: String,
     email: String
 });
 
+//exporting userSchema
 module.exports = mongoose.model('User', userSchema);
 
 
